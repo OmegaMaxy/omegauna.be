01daf52a7ef71bec48112433afb74560ae89c56e
Dazzy <41763728+OmegaMaxy@users.noreply.github.com>
2025-04-01 13:35:17 +0200

    Finishing touches

--- js/a3cd1f.js
+++ js/a3cd1f.js
@@ -3,10 +3,10 @@ const RESEND_API_KEY = 're_W33GQU8f_4fhUEzM5g79woWtt2tCvW792';
 async function a3cd1f_send() {
 
 
-    const email = document.getElementById('email');
-    const fullname = document.getElementById('fullname');
-    const tel = document.getElementById('tel');
-    const message = document.getElementById('message');
+  const email = document.getElementById('email');
+  const fullname = document.getElementById('fullname');
+  const tel = document.getElementById('tel');
+  const message = document.getElementById('message');
 
   const res = await fetch('https://api.resend.com/emails', {
     method: 'POST',
@@ -24,6 +24,44 @@ async function a3cd1f_send() {
             Email: <strong>${email}</strong>
             Message: <p>${message}</p>
         </main>`,
+    }),
+  });
+
+  if (res.ok) {
+    const data = await res.json();
+    return Response.json(data);
+  }
+}
+async function a3cd1f_full_send() {
+
+
+  const email = document.getElementById('email');
+  const fullname = document.getElementById('fullname');
+  const tel = document.getElementById('tel');
+  const message = document.getElementById('message');
+  const inquiry = document.getElementById('inquiry');
+  const btw = document.getElementById('btw');
+  const company_name = document.getElementById('company_name');
+  
+  const res = await fetch('https://api.resend.com/emails', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${RESEND_API_KEY}`,
+    },
+    body: JSON.stringify({
+      from: 'Acme <form@omegauna.be>',
+      to: [email],
+      subject: `Form OmegaUna: ${fullname}`,
+      html: `
+      <main>
+          Tel: <strong>${tel}</strong>
+          Email: <strong>${email}</strong>
+          Company: <strong>${company_name}</strong>
+          BTW: <strong>${btw}</strong>
+          Inquiry: <strong>${inquiry}</strong>
+          Message: <p>${message}</p>
+      </main>`,
     }),
   });
 
